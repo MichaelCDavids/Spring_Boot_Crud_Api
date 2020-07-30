95473290b3b2f998d11404c165fbce0235cfcf56
MichaelCDavids <michaelcdavids@gmail.com>
2020-07-30 18:21:56 +0200

    initial commit

---
+++ src/main/java/net/code/ezingo/EzingoApplication.java
@@ -0,0 +1,13 @@
+package net.code.ezingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/code/ezingo/EzingoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.code.ezingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
