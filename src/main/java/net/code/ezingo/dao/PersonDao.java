0ed06aa6cb412b1f2cffb92aa32ffa0160b8dfd7
MichaelCDavids <michaelcdavids@gmail.com>
2020-07-30 19:48:55 +0200

    interface for persons class database transactions

---
+++ src/main/java/net/code/ezingo/dao/PersonDao.java
@@ -0,0 +1,26 @@
+package net.code.ezingo.dao;
+
+import net.code.ezingo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    /**
+     * Inserts a person into the database
+     * @param id -  unique user id for person
+     * @param person - person to be added to database
+     * @return int - indicates if user was added to database
+     */
+    int insertPerson(UUID id, Person person);
+
+    /**
+     * The addPerson methods adds a user to the database generating a UUID for each Person
+     * @param person - person to be inserted into the database
+     * @return integer - indicates if user was added to database
+     */
+    default int addPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
