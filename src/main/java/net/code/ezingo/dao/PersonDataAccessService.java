b036d6a7a90964981d8446c37cf8839709677d53
MichaelCDavids <michaelcdavids@gmail.com>
2020-08-01 13:42:54 +0200

    depency injection for postgres

---
+++ src/main/java/net/code/ezingo/dao/PersonDataAccessService.java
@@ -0,0 +1,41 @@
+package net.code.ezingo.dao;
+
+import net.code.ezingo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Repository("postgres")
+public class PersonDataAccessService  implements PersonDao{
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        List<Person> listOfPerson = new ArrayList<>();
+        listOfPerson.add(new Person(UUID.randomUUID(),"Muji"));
+        return  listOfPerson;
+    }
+
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
